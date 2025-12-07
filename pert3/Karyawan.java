2c5dfac7b9e9c9781a0174f7f5242b00b6384ce5
bilxdi <biladi.amna@mhs.unsoed.ac.id>
2025-12-07 21:43:43 +0700

    pert2 don

--- pert3/Karyawan.java
+++ pert3/Karyawan.java
@@ -3,13 +3,17 @@ package pert3;
 class Karyawan {
     // Deklarasikan variabel/state dasar (nama, gajiPokok)
     // Gunakan access modifier yang tepat agar bisa diakses oleh subclass (misal: protected)
-    
+    protected String nama;
+    protected double gaji;
     
     // Constructor untuk inisialisasi nama dan gajiPokok
-    
-    
+    public Karyawan(String nama, double gaji) {
+        this.nama = nama;
+        this.gaji = gaji;
+    }
+
     // Method untuk menampilkan informasi dasar karyawan
     void tampilInfo() {
-        
+        System.out.print("Nama: " +nama+ " | Gaji Pokok: Rp " +gaji);
     }
 }
--- pert3/Manajer.java
+++ pert3/Manajer.java
@@ -1,18 +1,23 @@
 package pert3;
 
 // Gunakan keyword untuk mewarisi class Karyawan
-//class Manajer ... {
+class Manajer extends Karyawan {
     // Deklarasikan variabel tambahan khusus Manajer (tunjangan)
-    
+    double tunjangan;
     
     // Constructor
     // Tips: Gunakan 'super' untuk memanggil constructor parent
-    
-    
+    public Manajer(String nama, double gaji, double tunjangan) {
+        super(nama, gaji);
+        this.tunjangan = tunjangan;
+    }
+
     // Method Override tampilInfo
     // Tips: Tampilkan info dasar, lalu tambahkan info tunjangan dan total gaji
-//    @Override
-//    void tampilInfo() {
-        
-//    }
-//}
+    @Override
+    void tampilInfo() {
+        super.tampilInfo();
+        System.out.print(" | Tunjangan: Rp " +tunjangan);
+        System.out.println("\nTotal Pendapatan: Rp " +(gaji+tunjangan));
+    }
+}
